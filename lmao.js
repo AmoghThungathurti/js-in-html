0d3321edbfafc84d2292fdfa659573a7b6fb4031
AmoghThungathurti <36045681+AmoghThungathurti@users.noreply.github.com>
2018-03-22 09:58:53 -0400

    Update lmao.js

--- lmao.js
+++ lmao.js
@@ -1,9 +1,8 @@
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(10000, 1000);
 }
 
 function draw() {
-  background(220);
-	fill(0, 255, 0);
+	fill(mouseX, mouseY, 100);
 	ellipse(mouseX, mouseY, 100, 100);
 }
